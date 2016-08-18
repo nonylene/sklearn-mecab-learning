eb2c0db938709dcf082382fa945df723f61f02f2
nonylene <cookie.speaker@gmail.com>
2016-08-18 16:43:34 +0900

    refactoring etc

--- classifier.py
+++ classifier.py
@@ -1,122 +1,96 @@
 import json
 import MeCab
 from functools import reduce
+import itertools
 
-# TODO: refactoring
+from collections import Counter
 
 mecab = MeCab.Tagger('')
-with open("positive_recipe.json") as f:
-    # 材料
-    positive_words_s = []
-    for line in f.readlines():
-        j = json.loads(line)
-        names = []
-        if "ingredients" not in j:
-            continue
-        for name in list( map(lambda x: x["name"], j["ingredients"])):
-            # なぜかエラーがでるので
-            try:
-                for word in filter(lambda x: "\t名詞," in x, mecab.parse(name).split("\n")):
-                     names.append(word.split("\t")[0])
-            except:
+
+# 半分テスト、半分学習用
+# return pair
+def _create_train_and_test_words(filename):
+    with open(filename) as f:
+        words_s = []
+        test_words_s = []
+        i = 0
+        for line in f.readlines():
+            j = json.loads(line)
+            names = []
+            if "ingredients" not in j:
                 continue
-        positive_words_s.append(names)
+            l = [x["name"] for x in j["ingredients"]]
+            l.append(j["title"])
+            for name in l:
+                # たまにエラーがでるので
+                try:
+                    for word in filter(lambda x: "\t名詞,一般" in x or "\t名詞,固有名詞" in x, mecab.parse(name).split("\n")):
+                         names.append(word.split("\t")[0])
+                except:
+                    continue
+            if i % 2 == 0:
+                words_s.append(names)
+            else:
+                test_words_s.append(names)
+            i += 1
+        return (words_s, test_words_s)
 
-# 頻度が少ないものを取り除く
-
-positive_counts = {}
-
-for positive_words in positive_words_s:
-    for word in positive_words:
-        if word in positive_counts:
-            positive_counts[word] += 1
-        else:
-            positive_counts[word] = 1
-
-for c in list(positive_counts):
-    if positive_counts[c] < 30:
-        del positive_counts[c]
-
-
-with open("negative_recipe.json") as f:
-    # 材料
-    negative_words_s = []
-    for line in f.readlines():
-        j = json.loads(line)
-        names = []
-        if "ingredients" not in j:
+def _create_vector_from_word(original_words, vector_words):
+    vector = [0] * len(original_words)
+    for word in vector_words:
+        try:
+            vector[original_words.index(word)] += 1
+        except:
             continue
-        for name in list( map(lambda x: x["name"], j["ingredients"])):
-            # なぜかエラーがでるので
-            try:
-                for word in filter(lambda x: "\t名詞," in x, mecab.parse(name).split("\n")):
-                     names.append(word.split("\t")[0])
-            except:
-                continue
-        negative_words_s.append(names)
+    return vector
 
-# 頻度が少ないものを取り除く
+positive_words_s, test_positive_words_s = _create_train_and_test_words("positive_recipe.json")
 
-negative_counts = {}
+positive_counts = Counter(itertools.chain.from_iterable(positive_words_s)).most_common(200)
 
-for negative_words in negative_words_s:
-    for word in negative_words:
-        if word in negative_counts:
-            negative_counts[word] += 1
-        else:
-            negative_counts[word] = 1
+negative_words_s, test_negative_words_s = _create_train_and_test_words("negative_recipe.json")
 
-for c in list(negative_counts):
-    if negative_counts[c] < 700:
-        del negative_counts[c]
+negative_counts = Counter(itertools.chain.from_iterable(negative_words_s)).most_common(200)
 
-words = list(set(list(positive_counts) + list(negative_counts)))
-
-print(len(words))
+words = list(set([x[0] for x in positive_counts] + [x[0] for x in negative_counts]))
 
 # word list
 
-word_vectors = []
-word_bools = []
+test_word_vectors = []
+test_word_bools = []
+
 fit_word_vectors = []
 fit_word_bools = []
 
-for i in range(len(positive_words_s)):
+for i in range(0, len(positive_words_s)):
     positive_words = positive_words_s[i]
-    vector = [0] * len(words)
-    for word in positive_words:
-        if word in words:
-            vector[words.index(word)] += 1
-    if i % 2 == 0:
-        fit_word_vectors.append(vector)
-        fit_word_bools.append(1)
-    word_vectors.append(vector)
-    word_bools.append(1)
+    fit_word_vectors.append(_create_vector_from_word(words, positive_words))
+    fit_word_bools.append(1)
 
-for i in range(len(negative_words_s)):
+for i in range(0, len(negative_words_s), 2):
     negative_words = negative_words_s[i]
-    vector = [0] * len(words)
-    for word in negative_words:
-        if word in words:
-            vector[words.index(word)] += 1
-    if i % 7 == 0:
-        fit_word_vectors.append(vector)
-        fit_word_bools.append(0)
-    if i % 15 == 0:
-        word_vectors.append(vector)
-        word_bools.append(0)
+    fit_word_vectors.append(_create_vector_from_word(words, negative_words))
+    fit_word_bools.append(0)
 
-print("ok")
+for test_positive_words in test_positive_words_s:
+    test_word_vectors.append(_create_vector_from_word(words, test_positive_words))
+    test_word_bools.append(1)
+
+for i in range(0, len(test_negative_words_s), 15):
+    test_negative_words = test_negative_words_s[i]
+    test_word_vectors.append(_create_vector_from_word(words, test_negative_words))
+    test_word_bools.append(0)
+
+print("test data create ok")
 
 from sklearn import svm
 clf = svm.SVC()
 clf.fit(fit_word_vectors, fit_word_bools)
 c = 0
-for vector, boo in zip(word_vectors, word_bools):
+for vector, boo in zip(test_word_vectors, test_word_bools):
     a = clf.predict([vector])
     if a[0] == boo:
         c += 1
 
-print(c)
-print(c / len(word_vectors))
-
+print("ok count: {0}".format(c))
+print("accuracy: {0}".format(c / len(test_word_vectors)))
